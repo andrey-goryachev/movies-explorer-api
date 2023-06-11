2a349f59aa72b0484876c10d21f894575da72c35
Andrey Goryachev <goryachev.andrej@yandex.ru>
2023-06-11 21:56:34 +0300

    create models

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-const PORT = 3000;
+const {
+  DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  PORT = 3000,
+} = process.env;
+
 const app = express();
+mongoose.connect(DB_ADDRESS, { autoIndex: true });
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
